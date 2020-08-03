1132fd77a234642216e568d544c1f067d1159b2c
Steven Zimmer <webdevzim@gmail.com>
2020-08-03 07:52:33 -0700

    update versions

--- wp-content/themes/fictiv-theme/functions.php
+++ wp-content/themes/fictiv-theme/functions.php
@@ -8,9 +8,9 @@
         
         wp_enqueue_script( 'mkto-forms', '//app-ab20.marketo.com/js/forms2/js/forms2.min.js', [], '1.1', false );
 
-        wp_enqueue_script('main-js', get_template_directory_uri() . '/dist/main/js/scripts.min.js', ['mkto-forms'], '1.0', true);
+        wp_enqueue_script('main-js', get_template_directory_uri() . '/dist/main/js/scripts.min.js', ['mkto-forms'], '1.1', true);
     
-        // wp_enqueue_style('style', get_template_directory_uri() . '/dist/main/css/style.min.css', [], '1.2');
+        wp_enqueue_style('style', get_template_directory_uri() . '/dist/main/css/style.min.css', [], '1.3');
     }
 
     function primary_button( $text = 'get a quote' ) {
