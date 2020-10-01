b0ee43345bb848db24fddd66771e41a5c8e75079
Steven Zimmer <webdevzim@gmail.com>
2020-10-01 06:32:49 -0700

    scripts removal

--- wp-content/themes/fictiv-theme/header.php
+++ wp-content/themes/fictiv-theme/header.php
@@ -16,23 +16,23 @@
       	fa('init',{product:'dotcom'});fa('pageview');fa('tracklinks');
     </script>
     
-  	<script async src='https://app.fictiv.com/analytics.js'></script>
+  	<!-- <script async src='https://app.fictiv.com/analytics.js'></script> -->
 
 </head><!--/head-->
 
 <!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+<!-- <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TVR9RRN');</script>
+})(window,document,'script','dataLayer','GTM-TVR9RRN');</script> -->
 <!-- End Google Tag Manager -->
 
 <body <?php body_class('overflow-x-hidden pt-0 lg:pt-18' ); ?>>
 
 <!-- Google Tag Manager (noscript) -->
-<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVR9RRN"
-height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TVR9RRN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript> -->
 <!-- End Google Tag Manager (noscript) -->
 
 <?php 	
@@ -44,14 +44,13 @@ height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 	if ( wp_is_mobile() ) :
 
 		// Show mobile filter on Resource center pages
-		if( is_search() || 
-
+		if( 
+			is_search() || 
 			is_page_template('page-filter.php') || 
 			is_archive() ||
 			is_page_template('page-resource-center.php') ||
 			is_singular('cpt_blog') ||
 			is_singular('cpt_teardown') 
-		
 		) :
 
 			include( get_template_directory() . '/inc/post-taxonomies.php');
