e03d98b4def7c613c1f085ef2c0c563ede3fc906
Steven Zimmer <webdevzim@gmail.com>
2020-10-01 17:04:40 -0700

    versions update

--- wp-content/themes/fictiv-theme/functions.php
+++ wp-content/themes/fictiv-theme/functions.php
@@ -10,7 +10,7 @@
 
         wp_enqueue_script('main-js', get_template_directory_uri() . '/dist/main/js/scripts.min.js', ['mkto-forms'], '1.7', true);
     
-        wp_enqueue_style('style', get_template_directory_uri() . '/dist/main/css/style.min.css', [], '1.29');
+        wp_enqueue_style('style', get_template_directory_uri() . '/dist/main/css/style.min.css', [], '1.30');
     }
 
     function primary_button( $text = 'get a quote' ) {
