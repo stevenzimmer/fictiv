672cdff0e28525b12b29ae561497163923b04587
Steven Zimmer <webdevzim@gmail.com>
2020-10-03 04:18:16 -0700

    Cookies in progress

--- wp-content/themes/fictiv-theme/assets/js/cookies/_utm-parameters.js
+++ wp-content/themes/fictiv-theme/assets/js/cookies/_utm-parameters.js
@@ -5,12 +5,15 @@ import { createCookie } from './create-cookie';
 
 // var url_string = "http://www.example.com/t.html?a=1&b=3&c=m2-m3-m4-m5"; //window.location.href
 var url = new URL( location.href );
-var utmContent = url.searchParams.get("utm_content");
+
 // console.log(url);
 // console.log(utmContent);
-if ( utmContent ) {
-	createCookie('utm_content', utmContent, 1);	
-}
+
+// var utmContent = url.searchParams.get("utm_content");
+
+// if ( !readCookie('utm_content') ) {
+// 	createCookie( 'utm_content', utmContent, 1 );	
+// }
 
 
 // console.log( readCookie('utm_content') );
