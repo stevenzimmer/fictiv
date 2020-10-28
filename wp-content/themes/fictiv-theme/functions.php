4bacf364de5acd06e2443432cdb61faa868f0368
Steven Zimmer <webdevzim@gmail.com>
2020-10-28 11:32:21 -0700

    update js version

--- wp-content/themes/fictiv-theme/functions.php
+++ wp-content/themes/fictiv-theme/functions.php
@@ -11,7 +11,7 @@
 
         // wp_enqueue_script( 'mkto-forms', '//info.fictiv.com/js/forms2/js/forms2.min.js', [], '1.1', false );
         
-        wp_enqueue_script('main-js', get_template_directory_uri() . '/dist/main/js/scripts.min.js', ['mkto-forms'], '1.10', true);
+        wp_enqueue_script('main-js', get_template_directory_uri() . '/dist/main/js/scripts.min.js', ['mkto-forms'], '1.11', true);
 
     
         wp_enqueue_style('style', get_template_directory_uri() . '/dist/main/css/style.min.css', [], '1.44');
