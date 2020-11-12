d07b379a686b903145e1994ed68aafa2b4e662b5
Steven Zimmer <webdevzim@gmail.com>
2020-11-12 15:48:45 -0700

    social posts tagged

--- wp-content/themes/fictiv-theme/partials/social-post.php
+++ wp-content/themes/fictiv-theme/partials/social-post.php
@@ -5,18 +5,18 @@ include( get_template_directory() . '/inc/post-taxonomies.php');
 	<div class="flex justify-between items-center">
 		<a title="Share <?php the_title()?> on Twitter" rel="noopener noreferrer" target="_blank" class="js-share-twitter-link" href="https://twitter.com/intent/tweet?text=<?php the_title(); ?>&url=<?php echo urlencode( get_the_permalink() ); ?>&hashtags=<?php echo $topic_name; ?>&via=fictiv">
 		<?php 
-			echo file_get_contents( get_template_directory_uri() . '/assets/images/icons/twitter.svg');
+			echo file_get_contents( get_template_directory_uri() . '/assets/images/icons/twitter.svg?1');
 		?>
 			
 		</a>
 			<a title="Share <?php the_title()?> on Facebook" rel="noopener noreferrer" target="_blank" class="js-share-facebook-link" href="https://www.facebook.com/dialog/share?app_id=1688841191297210&display=popup&href=<?php echo urlencode( get_the_permalink() ); ?>&redirect_uri=<?php echo urlencode( get_the_permalink() ); ?>">
 				<?php 
-				echo file_get_contents( get_template_directory_uri() . '/assets/images/icons/facebook.svg');
+				echo file_get_contents( get_template_directory_uri() . '/assets/images/icons/facebook.svg?1');
 			?>
 	
 		</a>
 		<a title="Share <?php the_title()?> on LinkedIn" rel="noopener noreferrer" target="_blank" class="js-share-linkedin-link" href="https://www.linkedin.com/shareArticle/?mini=true&url=<?php echo urlencode( get_the_permalink() ); ?>&title=<?php the_title(); ?>"><?php 
-			echo file_get_contents( get_template_directory_uri() . '/assets/images/icons/linkedin.svg');
+			echo file_get_contents( get_template_directory_uri() . '/assets/images/icons/linkedin.svg?1');
 		?></a>
 	</div>
 </div>
